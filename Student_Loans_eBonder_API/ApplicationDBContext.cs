61678829e4514730a99f3fe09102f6ca06e56d79
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-09-19 02:21:07 +0200

    Add Students DbSet to ApplicationDBContext
    - Introduced DbSet<Student.Types.Models.Student> to ApplicationDBContext to support student management within the application
    - This addition enhances the database schema by allowing for direct access to Student entities, improving data organization and retrieval

--- Student_Loans_eBonder_API/ApplicationDBContext.cs
+++ Student_Loans_eBonder_API/ApplicationDBContext.cs
@@ -47,5 +47,6 @@ internal class ApplicationDBContext : IdentityDbContext<User, Role, string, User
 
 	public DbSet<Name> Names { get; set; }
 	public DbSet<NameComponent> NameComponents { get; set; }
+	public DbSet<Student.Types.Models.Student> Students { get; set; }
 	public DbSet<UserProfile> UserProfiles { get; set; }
 }
