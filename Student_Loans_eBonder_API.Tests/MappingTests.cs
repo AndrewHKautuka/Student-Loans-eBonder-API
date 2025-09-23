56bbaca9de04b3fbcb514e6f1508a92f1ed47cab
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-09-23 08:24:34 +0200

    Add unit tests for Mapster mappings
    - Implemented test to validate the mapping between RegisterStudentRequest and RegisterUserCommand
    - Implemented test to validate the mapping RegisterUserCommand to a User tuple

---
+++ Student_Loans_eBonder_API.Tests/MappingTests.cs
@@ -0,0 +1,41 @@
+
+using Mapster;
+
+using Student_Loans_eBonder_API.Auth.Types.Models;
+using Student_Loans_eBonder_API.Auth.Types.Requests;
+using Student_Loans_eBonder_API.Common;
+
+using Xunit.Abstractions;
+
+namespace Student_Loans_eBonder_API.Tests;
+
+public class MappingTests(ITestOutputHelper output) : IAsyncLifetime
+{
+	public async Task InitializeAsync()
+	{
+		MapsterConfig.SetupMapsterAdapterConfig();
+	}
+
+	public async Task DisposeAsync() { }
+
+	[Fact]
+	public void MapsterMapping_WhenMappingRegisterStudentCommandToRegisterUserCommand_ShouldCreateValidRegisterUserCommand()
+	{
+		var source = new RegisterStudentRequest { Email = "test@example.com", Password = "1234!@#$" };
+		var dest = source.Adapt<RegisterUserCommand>();
+
+		Assert.Equal("test@example.com", dest.Email);
+		Assert.Equal("1234!@#$", dest.Password);
+	}
+
+	[Fact]
+	public void MapsterMapping_WhenMappingRegisterUserCommandToUser_ShouldCreateValidUserAndPassword()
+	{
+		var source = new RegisterUserCommand { Email = "test@example.com", Password = "1234!@#$" };
+		var dest = source.Adapt<(User User, string Password)>();
+
+		Assert.Equal("test@example.com", dest.User.Email);
+		Assert.Equal("test@example.com", dest.User.UserName);
+		Assert.Equal("1234!@#$", dest.Password);
+	}
+}
