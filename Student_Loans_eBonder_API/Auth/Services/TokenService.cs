19dd22cb04d0ad9bc3e7b1304df35e6b60a38815
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-02 20:32:09 +0200

    Add TokenService for JWT token generation
    - Implemented TokenService class to handle the creation of JWT access tokens
    - Added methods for building access tokens and logging token generation attempts and successes
    - Utilized configuration for JWT key and set token expiration to one month

---
+++ Student_Loans_eBonder_API/Auth/Services/TokenService.cs
@@ -0,0 +1,55 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using Microsoft.IdentityModel.Tokens;
+
+using Student_Loans_eBonder_API.Auth.Types.Commands;
+using Student_Loans_eBonder_API.Auth.Types.Responses;
+
+namespace Student_Loans_eBonder_API.Auth.Services;
+
+public partial class TokenService
+{
+	private readonly ILogger<TokenService> _logger;
+	private readonly IConfiguration _configuration;
+
+	public TokenService(ILogger<TokenService> logger, IConfiguration configuration)
+	{
+		_logger = logger;
+		_configuration = configuration;
+	}
+
+	public BuildAccessTokenResponse BuildAccessToken(BuildAccessTokenCommand buildAccessTokenCommand)
+	{
+		LogBuildTokenAttemptMessage(_logger, buildAccessTokenCommand.UserId);
+
+		var claims = new List<Claim>()
+		{
+			new ("userId", buildAccessTokenCommand.UserId)
+		};
+
+		var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTKey"]!));
+		var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+		var expires = DateTime.UtcNow.AddMonths(1);
+
+		var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expires, signingCredentials: creds);
+
+		var response = new BuildAccessTokenResponse
+		{
+			Token = new JwtSecurityTokenHandler().WriteToken(token),
+			Expires = expires,
+		};
+
+		LogBuildTokenSuccessMessage(_logger, buildAccessTokenCommand.UserId);
+
+		return response;
+	}
+
+	[LoggerMessage(Level = LogLevel.Information, Message = "Attempt to build token for user {UserId}")]
+	static partial void LogBuildTokenAttemptMessage(ILogger logger, string userId);
+
+	[LoggerMessage(Level = LogLevel.Information, Message = "Successfully built token for user {UserId}")]
+	static partial void LogBuildTokenSuccessMessage(ILogger logger, string userId);
+}
