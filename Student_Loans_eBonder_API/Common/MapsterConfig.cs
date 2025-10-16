fbb9c3039f2df0cb91c6b86b07ab9dc3d9dde78e
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-16 07:10:48 +0200

    Updated Mapster config to be thread-safe setup
    - Implemented a locking mechanism in the SetupMapsterAdapterConfig method to ensure that the configuration is only set up once, preventing potential race conditions in multi-threaded scenarios
    - Introduced a private static boolean flag to track whether the setup has already been completed, enhancing the efficiency and safety of the configuration process

--- Student_Loans_eBonder_API/Common/MapsterConfig.cs
+++ Student_Loans_eBonder_API/Common/MapsterConfig.cs
@@ -7,11 +7,29 @@ namespace Student_Loans_eBonder_API.Common;
 
 public static class MapsterConfig
 {
+	private static bool _hasBeenSetup;
+	private static readonly Lock _setupLock = new();
+
 	public static void SetupMapsterAdapterConfig()
 	{
-		TypeAdapterConfig<RegisterUserCommand, (User User, string Password)>.NewConfig()
-			.Map(dest => dest.User, src => src)
-			.Map(dest => dest.User.UserName, src => src.Email)
-			.Map(dest => dest.Password, src => src.Password);
+		if (_hasBeenSetup)
+		{
+			return;
+		}
+
+		lock (_setupLock)
+		{
+			if (_hasBeenSetup)
+			{
+				return;
+			}
+
+			TypeAdapterConfig<RegisterUserCommand, (User User, string Password)>.NewConfig()
+				.Map(dest => dest.User, src => src)
+				.Map(dest => dest.User.UserName, src => src.Email)
+				.Map(dest => dest.Password, src => src.Password);
+
+			_hasBeenSetup = true;
+		}
 	}
 }
